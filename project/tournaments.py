0b1048c68b34801c8a9907dbe6a3376bac1dc458
Alexandre Goyo <alexandregoyo@gmail.com>
2022-01-02 23:38:37 -0300

    atualização em tournaments.py

--- project/tournaments.py
+++ project/tournaments.py
@@ -1,6 +1,8 @@
 import random
 from project import Player
 
+import copy  # importing to create a deepcopy of kind_results to be able to alter the mutable separately
+
 
 class Tournament(Player):
 
@@ -17,19 +19,33 @@ class Tournament(Player):
 
     @table.setter
     def table(self, names):
-        self._table.update({i: self.kind_results.copy() for i in names})
+        self._table.update({i: copy.deepcopy(self.kind_results) for i in names})
+
+    def checking_players(self, name1, name2):
+        if name1 in self._table:
+            if name2 in self._table:
+                return True
+        elif name1 not in self._table:
+            return f'O Jogador {name1} não foi registrado'
+        elif name2 not in self._table:
+            return f'O Jogador {name2} não foi registrado'
+        else:
+            return f'Nenhum Jogador foi encontrado'
 
     @staticmethod
-    def setting_results(self, v_player, l_player, result_of_players):
-        if v_player & l_player in self._table:
-            self._table[v_player]['WIN'] += {l_player: result_of_players}
-            self._table[l_player]['LOSSES'] += {v_player: result_of_players.reverse()}
-        elif v_player not in self._table:
-            print(f'O Jogador {v_player} não foi registrado!')
-        elif l_player not in self._table:
-            print(f'O Jogador {l_player} não foi registrado!')
+    def setting_won_losses(self, v_player, l_player):
+        if Tournament.checking_players(self, v_player, l_player) is True:
+            self._table[v_player]['WIN'].append(l_player)
+            self._table[l_player]['LOSSES'].append(v_player)
         else:
-            print(f'Nenhum Jogador encontrado!')
+            print(Tournament.checking_players(self, v_player, l_player))
+
+    def setting_draw(self, player1, player2):
+        if Tournament.checking_players(self, player1, player2) is True:
+            self._table[player1]['Draw'].append(player2)
+            self._table[player2]['Draw'].append(player1)
+        else:
+            print(Tournament.check(self, player1, player2))
 
     def getting_victory(self, name):
         if name in self._table:
@@ -48,8 +64,12 @@ class Swiss(Tournament):
     def __getitem__(self, item):
         return self._table[item]
 
+    def byes(self):
+        pass
+
     @staticmethod
     def first_bye(self):
         if len(self._table) % 2 != 0:
-            z = random.sample(self._table.key, 1)
-            return z
+            z = random.sample(list(self._table), 1)
+            print(f'O Jogador{z[0]} está de BYE')
+            return z[0]
